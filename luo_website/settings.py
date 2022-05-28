0d4bcb7cbac1f402237f0a6db5f7c1d77507d009
luo hao <30183575@qq.com>
2022-05-28 19:22:18 +0800

    Revert "uodate"
    This reverts commit 5b0809ebe2c9b082066b1c882b4f34eb09d9e0f9.

--- luo_website/settings.py
+++ luo_website/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
-try:
-    from luo_website.local_settings import*
-except ImportError:
-    print('No local_settings')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,9 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-p$_#+@yk$m+#=4cq7w^z3qj=8_pv@--w&e0d!)dq3y0m1h5)+6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -138,3 +137,9 @@ Q_CLUSTER = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'my_cache_table',
+    }
+}
